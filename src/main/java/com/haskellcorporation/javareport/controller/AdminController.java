b1a4ebc1d1bd4730a1c6609cb2e2d9998ccc6e42
blinkofaneye <mratiarison41@gmail.com>
2020-04-14 11:34:04 +0300

    Add dashboard template

--- src/main/java/com/haskellcorporation/javareport/controller/AdminController.java
+++ src/main/java/com/haskellcorporation/javareport/controller/AdminController.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class AdminController {
     @GetMapping("/dashboard")
     public String test() {
-        return "dist/custom/pages/user/dashboard";
+        return "dist/dashboard";
     }
 }
